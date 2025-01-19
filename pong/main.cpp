65b65b44e40d0e053636a7d399133897b792e3b4
Stipax27 <stepanpetuskin@gmail.ru>
2025-01-19 16:25:48 +0300

    test

--- pong/main.cpp
+++ pong/main.cpp
@@ -17,7 +17,7 @@ float starSize = 10;
 sprite racket;//ракетка игрока
 sprite enemy;//ракетка противника
 sprite ball;//шарик
-//
+
 struct {
     int score, balls;//количество набранных очков и оставшихся "жизней"
     bool action = false;//состояние - ожидание (игрок должен нажать пробел) или игра
