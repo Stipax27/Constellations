c7df714724f65a4153f377b64a53a69402276593
Student-2 GameSchool <stud2.gsch124@outlook.com>
2025-03-04 15:58:20 +0300

    add player inf WIP

---
+++ pong/Player.h
@@ -0,0 +1,20 @@
+class Player 
+{
+public:
+};
+enum Elements
+{ Fire, Earth, Air, Water
+};
+enum Trups
+{
+	BlackHole, Quasar, Nebula
+};
+enum TimeWorld
+{
+	Hourglass
+};
+enum Colors
+{
+	Red, orange, yellow, green, blue, indigo, violet
+};
+
