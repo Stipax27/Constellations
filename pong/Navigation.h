aadc2948961a93e49bd8562a95ec362a7ffad9cb
Proxima prime <Proxima prime@DESKTOP-4OAR5T1>
2025-04-04 22:06:47 +0700

    add WASD movements in fight

--- pong/Navigation.h
+++ pong/Navigation.h
@@ -1,105 +1,152 @@
 
 int start;
-bool isMoveActive = false;
+
+bool isMoveActive = false; 
+
 
 float finalS;
-			
+
+char whatKeyPressed() {
+	if (GetAsyncKeyState('W') & 0x0001) {
+		//TextOutA(window.context, window.width / 2 + 20, window.height / 2 + 50., "Работаем!", 20);
+
+		return 'W';
+	}
+	if (GetAsyncKeyState('A') & 0x0001)
+		return 'A';
+	if (GetAsyncKeyState('S') & 0x0001)
+		return 'S';
+	if (GetAsyncKeyState('D') & 0x0001)
+		return 'D';
+	return '\0';  // Если ничего не нажато, возвращаем пустой символ
+}
 
 void fightMove(point3d& p) {
-
 
 	float T;
 	float moveTimer = 4;
 	float currentV;
 	float currentS;
 	float currentA;
-	float F = -2.5;
+	float F = -1.5;
 	bool isMoveStateBack;
 
-	if (GetAsyncKeyState('W') & 0x0001) // Проверка срабатывает если кнопка нажимается хотя бы один раз
+	static char SYM[2] = {0};
+
+	char key = whatKeyPressed();
+
+	if (!isMoveActive && key != '\0') // Проверка срабатывает если кнопка нажимается хотя бы один раз
 	{
+	//*SYM = { whatKeyPressed() };
+		//if (!A || !S || !D) {
+				//W = true;
 		if (!isMoveActive)
 		{
+			//SYM[0] = 'A';
+			SYM[0] = key;
+			SYM[1] = '\0';
+
+
 			start = timeGetTime();
 			isMoveActive = true;
 		}
+		//}
 	}
 
-
 	if (isMoveActive) {
-
 		T = timeGetTime() - start;
-
 		currentA = F * (moveTimer / 2 - T / 1000);
-
 		currentV = currentA * T / 1000;
 		
 		currentS = currentV * T / 1000;
-
 		isMoveStateBack = false;
-
 		if (T/1000 < moveTimer) {
-
-
 			if (T/1000 > (moveTimer / 2))
 			{
 				isMoveStateBack = true;
 			}
-
 			if (!isMoveStateBack) {
-				move(p, 0, currentS, 0);
 				finalS = currentS;
+				switch (SYM[0]) {
+				case 'W':
+					move(p, 0, currentS, 0);
+					break;
+				case 'A':
+					move(p, currentS, 0, 0);
+					break;
+				case 'S':
+					move(p, 0, -currentS, 0);
+					break;
+				case 'D':
+					move(p, -currentS, 0, 0);
+					break;
+
+
+				}
+				//move(p, 0, currentS, 0);
 			}
 			else {
 				currentA = F * (T / 1000 - moveTimer / 2);
-
 				//F = 1;
 				//currentA = -currentA;
-
 				//move(p, 0, 2 * finalS - currentS, 0);
 			}
-
-
 			//
-			char temp[7] = {0};
+			char temp[64] = {0};
 			//_ultoa_s(moveTimer - T / 1000, temp, 10);//преобразование числовой переменной в текст. текст окажется в переменной txt
+			//_ultoa_s(currentA, temp, 10);//преобразование числовой переменной в текст. текст окажется в переменной txt
+			//_ultoa_s(moveTimer - T / 1000, temp, 10);//преобразование числовой переменной в текст. текст окажется в переменной txt
+			//TextOutA(window.context, window.width / 2, window.height / 2., (LPCSTR)temp, strlen(temp));
+		}
+		else {
+			isMoveActive = false;
+		}
+	}
+}
 
-			//_ultoa_s(currentA / 1000, temp, 10);//преобразование числовой переменной в текст. текст окажется в переменной txt
+//
 
-			_ultoa_s(moveTimer - T / 1000, temp, 10);//преобразование числовой переменной в текст. текст окажется в переменной txt
+	/*
+	if (isMoveActive) {
+
+		T = timeGetTime() - start;
+
+		if (T / 1000 < 3) {
+			char temp[64] = { 0 };
+
+
+			_ultoa_s(T / 1000, temp, 10);//преобразование числовой переменной в текст. текст окажется в переменной txt
 			TextOutA(window.context, window.width / 2, window.height / 2., (LPCSTR)temp, strlen(temp));
+			TextOutA(window.context, window.width / 2 + 20, window.height / 2., SYM, 1);
 
 		}
 		else {
 			isMoveActive = false;
 		}
 	}
+	*/
 	
-
-
 
 ////////////////////////////
 
 
+	//if (GetAsyncKeyState('A'))
+	//{
+	//	move(p, currentV, 0, 0);
+	//};
 
+	//if (GetAsyncKeyState('S'))
+	//{
+	//	move(p, 0, -currentV, 0);
 
-	if (GetAsyncKeyState('A'))
-	{
-		move(p, currentV, 0, 0);
-	};
+	//};
 
-	if (GetAsyncKeyState('S'))
-	{
-		move(p, 0, -currentV, 0);
+	//if (GetAsyncKeyState('D'))
+	//{
+	//	move(p, -currentV, 0, 0);
 
-	};
+	//};
 
-	if (GetAsyncKeyState('D'))
-	{
-		move(p, -currentV, 0, 0);
-
-	};
-}
 
 
 
