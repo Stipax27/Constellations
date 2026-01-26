cb98688492c6fe96d886ce79e4bc5414b3b49353
gs <misobenin@gmail.com>
2026-01-26 17:57:01 +0700

    wip

--- dx11minimal/LevelManagerClass.cpp
+++ dx11minimal/LevelManagerClass.cpp
@@ -352,18 +352,18 @@ bool LevelManagerClass::Initialize()
 
 	// MAIN MENU //
 	
-	entity = m_World->CreateEntity();
-	transform = entity->AddComponent<Transform>();
-	//transform->position = point3d(0.5f, 0.1f, 0.0f);
-	transform->scale = point3d(0.25f, 0.05f, 0.0f);
-	rect = entity->AddComponent<Rect>();
-	rect->color = point3d(0.5f, 0.25f, 0.8f);
-	rect->opacity = 1.0f;
-	rect->anchorPoint = point3d(-1, 0, 0);
-	rect->ratio = ScreenAspectRatio::YY;
-	//rect->cornerRadius = 1.0f;
-	rect->cornerType = CornerType::Strict;
-	entity->AddComponent<Button>();
+	//entity = m_World->CreateEntity();
+	//transform = entity->AddComponent<Transform>();
+	////transform->position = point3d(0.5f, 0.1f, 0.0f);
+	//transform->scale = point3d(0.25f, 0.05f, 0.0f);
+	//rect = entity->AddComponent<Rect>();
+	//rect->color = point3d(0.5f, 0.25f, 0.8f);
+	//rect->opacity = 1.0f;
+	//rect->anchorPoint = point3d(-1, 0, 0);
+	//rect->ratio = ScreenAspectRatio::YY;
+	////rect->cornerRadius = 1.0f;
+	//rect->cornerType = CornerType::Strict;
+	//entity->AddComponent<Button>();
 
 	//entity = m_World->CreateEntity("Rect", entity);
 	//transform = entity->AddComponent<Transform>();
