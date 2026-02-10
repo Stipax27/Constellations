2eab49fcd2ae175f67039de28b869fd8df0fb400
Mr Bretten <misobenin@gmail.com>
2026-02-10 14:53:30 +0700

    wip

--- dx11minimal/spriteSystem.h
+++ dx11minimal/spriteSystem.h
@@ -15,8 +15,7 @@
 #include "Beam.h"
 
 #include "frustumclass.h"
-#include "Explosion.cpp"
-#include "Components/Orientation.h"
+#include "Explosion.cpp" 
 
 
 class SpriteSystem : public System
@@ -34,31 +33,6 @@ private:
 private:
 	XMMATRIX GetWorldMatrix(Transform);
 	void UpdateWorldMatrix(Transform);
-
-	static void ProcessParticle(Entity* entity, Transform& worldTransform);
-	static void EmitNewParticles(Entity* entity, const Transform& worldTransform, ParticleEmitter* emitter);
-	static double CalculateEmitDelta(const ParticleEmitter& emitter);
-	static void AddEmitterOrientation(Entity* entity, Transform& transform);
-	static int CalculateEmitCount(const ParticleEmitter& emitter, double elapsedTime, double emitDelta);
-	static Orientation ApplySpread(Orientation orientation, const ParticleEmitter& emitter);
-	static float GenerateRandomAngle(float spread);
-	static double CalculateParticleStartTime(const ParticleEmitter& emitter, double emitDelta, int particleIndex);
-	static void CreateParticle(const Transform& worldTransform, ParticleEmitter* emitter,
-	                           const Orientation& direction,
-	                           double startTime);
-	static void CreateReversedParticle(const Transform& worldTransform, ParticleEmitter* emitter,
-	                                   const Orientation& direction, double startTime);
-	static void CreateNormalParticle(const Transform& worldTransform, ParticleEmitter* emitter,
-	                                 const Orientation& direction, double startTime);
-	static void UpdateEmitTiming(ParticleEmitter* emitter, double emitDelta, int emittedCount);
-	static void UpdateExistingParticles(ParticleEmitter* emitter);
-
-	static void RenderParticles(const ParticleEmitter* emitter);
-	static void SetupShaders(const ParticleEmitter* emitter);
-	static void SetupConstantBuffers();
-	static void SetupParticleInfo(const ParticleEmitter* emitter);
-	static void SetupInputAssembler(const ParticleEmitter* emitter);
-	static Orientation CalculateEmitOrientation(const Orientation& baseOrientation, EmitDirection emitDir);
 };
 
 #endif
