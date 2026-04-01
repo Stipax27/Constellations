179aedcba02f61134feb535e402f35931d6441a1
Your Name <you@example.com>
2026-04-01 20:02:33 +0700

    save QuestGiver

---
+++ dx11minimal/QuestSystem.h
@@ -0,0 +1,28 @@
+#ifndef _QUEST_SYSTEM_H_
+#define _QUEST_SYSTEM_H_
+
+//////////////
+// INCLUDES //
+//////////////
+#include<cmath>
+#include "system.h"
+#include "QuestGiver.h"
+#include "Transform.h"
+#include "questmanager.h"
+
+
+class QuestSystem : public System
+{
+public:
+	QuestSystem();
+	void Initialize();
+	void Shutdown();
+
+	void Update(vector<Entity*>&, float);
+private:
+	Entity* player;
+	Transform* playerTransform;
+	QuestManager* questManager;
+};
+
+#endif
